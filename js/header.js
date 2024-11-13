b73a9983081ab3a44dfced9235a3adc07bfdb3b8
td.hoang <dinh-hoang.ta@seesat.eu>
2024-11-13 15:01:22 +0100

    Removed some unused stuff

--- js/header.js
+++
@@ -1,9 +0,0 @@
-// header.js
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('header.html') // Fetch the header content
-        .then(response => response.text()) // Convert to text (HTML)
-        .then(data => {
-            document.getElementById('header-container').innerHTML = data; // Inject into the container
-        })
-        .catch(error => console.error('Error loading header:', error)); // Handle any fetch errors
-});
